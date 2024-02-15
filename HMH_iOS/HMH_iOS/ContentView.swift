88495fdde4bfb2bd82f39d97e955ffb9e56b743c
Zoe <mam07065@naver.com>
2024-02-15 22:47:56 +0900

    [Chore] Init Project

---
+++ HMH_iOS/HMH_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HMH_iOS
+//
+//  Created by 지희의 MAC on 2/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HMH_iOS/HMH_iOS/HMH_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HMH_iOSApp.swift
+//  HMH_iOS
+//
+//  Created by 지희의 MAC on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct HMH_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
