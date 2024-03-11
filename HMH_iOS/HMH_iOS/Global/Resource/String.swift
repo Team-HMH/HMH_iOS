9f69dc98ec3c593f188be2d4834972917eeda71a
kim-seonwoo <sw204011@gmail.com>
2024-03-12 01:14:50 +0900

    [Add/#1] String literal 추가

--- HMH_iOS/HMH_iOS/Global/Resource/String.swift
+++ HMH_iOS/HMH_iOS/Global/Resource/String.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+
+enum StringLiteral {
+    enum TabBar {
+        static var challengeTitle = "챌린지"
+        static var homeTitle = "홈"
+        static var myPageTitle = "마이페이지"
+    }
+    
+    enum Home {
+        
+    }
+    
+    enum Challenge {
+        
+    }
+    
+    enum myPage {
+        
+    }
+    
+    enum AlertButton {
+        
+    }
+    
+    enum AlertTitle {
+
+    }
+    
+    enum AlertDescription {
+
+    }
+    
+    enum OnboardingButton {
+
+    }
+    
+    enum OnboardigMain {
+
+    }
+    
+    enum OnboardigSub {
+
+    }
+    
+    enum TimeSurveySelect {
+
+    }
+    
+    enum ProblemSurveySelect {
+
+    }
+    
+    enum PeriodSelect {
+
+    }
+}
