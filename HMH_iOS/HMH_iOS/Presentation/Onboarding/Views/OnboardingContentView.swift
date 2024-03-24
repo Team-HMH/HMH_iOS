f9583fae74af2fbf1e5a3a9624e71bc49c7123b3
kim-seonwoo <sw204011@gmail.com>
2024-03-24 20:36:05 +0900

    [Feat/#6] ProgressBar UI 추가

--- HMH_iOS/HMH_iOS/Presentation/Onboarding/Views/OnboardingContentView.swift
+++ HMH_iOS/HMH_iOS/Presentation/Onboarding/Views/OnboardingContentView.swift
@@ -8,10 +8,43 @@
 import SwiftUI
 
 struct OnboardingContentView: View {
+    
+    @StateObject
+    private var viewModel = OnboardingViewModel()
+    
+    @State
+    private var OnboardingState = 3
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            OnboardingProgressView(onboardingState: $OnboardingState)
+            //            SurveyContainerView()
+            //            NextButtonView()
+        }
+        .background(.blackground, ignoresSafeAreaEdges: .all)
     }
 }
+
+extension OnboardingContentView {
+    private func OnboardingProgressView(onboardingState: Binding<Int>) -> some View {
+        let totalSteps = 6
+        
+        return VStack {
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundColor(.gray3)
+                    .frame(width: 334, height: 4)
+                    .cornerRadius(1.0)
+                Rectangle()
+                    .foregroundColor(.bluePurpleLine)
+                    .frame(width: CGFloat(onboardingState.wrappedValue) / CGFloat(totalSteps) * 334, height: 4)
+                    .cornerRadius(10.0)
+                    .animation(.spring())
+            }
+        }
+    }
+}
+
 
 #Preview {
     OnboardingContentView()
