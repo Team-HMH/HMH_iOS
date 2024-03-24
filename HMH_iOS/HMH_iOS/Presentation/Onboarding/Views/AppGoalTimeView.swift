90ac97dfef55f7103f428825328ddd61891ebb5e
kim-seonwoo <sw204011@gmail.com>
2024-03-24 22:13:46 +0900

    [Add/#6] SurveyContainerView 관련 파일 추가

---
+++ HMH_iOS/HMH_iOS/Presentation/Onboarding/Views/AppGoalTimeView.swift
@@ -0,0 +1,18 @@
+//
+//  AppGoalTimeView.swift
+//  HMH_iOS
+//
+//  Created by Seonwoo Kim on 3/24/24.
+//
+
+import SwiftUI
+
+struct AppGoalTimeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppGoalTimeView()
+}
---
+++ HMH_iOS/HMH_iOS/Presentation/Onboarding/Views/SurveyProblemView.swift
@@ -0,0 +1,18 @@
+//
+//  SurveyProblemView.swift
+//  HMH_iOS
+//
+//  Created by Seonwoo Kim on 3/24/24.
+//
+
+import SwiftUI
+
+struct SurveyProblemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SurveyProblemView()
+}
