7b9d7bd9345eac01f2d2f9419cc901e650163b6f
kim-seonwoo <sw204011@gmail.com>
2024-04-02 14:13:29 +0900

    [Feat/#10] Picker UI 구현

--- HMH_iOS/HMH_iOS/Presentation/Onboarding/Views/GoalTimeView.swift
+++ HMH_iOS/HMH_iOS/Presentation/Onboarding/Views/GoalTimeView.swift
@@ -5,14 +5,91 @@
 //  Created by Seonwoo Kim on 3/24/24.
 //
 
+
 import SwiftUI
 
 struct GoalTimeView: View {
+    var colors = ["2", "3", "4", "5", "0"]
+    @State var selectedColor = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-           
+        HStack {
+            PickerExampleView(colors: colors, selectedColor: $selectedColor)
+                .frame(width: 67)
+            Text("시간")
+                .font(.text2_medium_20)
+                .foregroundColor(.gray2)
+        }
     }
 }
+
+struct PickerExampleView: UIViewRepresentable {
+    var colors: [String]
+    @Binding var selectedColor: String
+    
+    func makeUIView(context: Context) -> UIPickerView {
+        let pickerView = UIPickerView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        pickerView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        pickerView.dataSource = context.coordinator
+        pickerView.delegate = context.coordinator
+        
+        return pickerView
+    }
+    
+    func updateUIView(_ pickerView: UIPickerView, context: Context) {
+        if let selectedIndex = colors.firstIndex(of: selectedColor) {
+            pickerView.selectRow(selectedIndex, inComponent: 0, animated: true)
+        }
+    }
+    
+    func makeCoordinator() -> PickerCoordinator {
+        return PickerCoordinator(colors: colors, selectedColor: $selectedColor)
+    }
+}
+
+class PickerCoordinator: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
+    var colors: [String]
+    var selectedColor: Binding<String>
+    
+    init(colors: [String], selectedColor: Binding<String>) {
+        self.colors = colors
+        self.selectedColor = selectedColor
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row]
+    }
+     
+     func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+         let color = UIColor(.whiteText)
+         let font = UIFont(name: "Pretendard-Medium", size: 22)
+
+         
+         let attributes: [NSAttributedString.Key: Any] = [
+             NSAttributedString.Key(rawValue: NSAttributedString.Key.foregroundColor.rawValue): color,
+             NSAttributedString.Key(rawValue: NSAttributedString.Key.font.rawValue): font
+         ]
+         
+         return NSAttributedString(string: colors[row], attributes: attributes)
+     }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedColor.wrappedValue = colors[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 53
+    }
+}
+
 
 #Preview {
     GoalTimeView()
