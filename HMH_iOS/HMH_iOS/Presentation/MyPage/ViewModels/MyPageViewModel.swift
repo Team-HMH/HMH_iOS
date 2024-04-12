f49681bac569c215fe08191df096a5bce8fadacc
kim-seonwoo <sw204011@gmail.com>
2024-04-12 16:08:04 +0900

    [Add/#15] MyPage 관련 파일 추가

---
+++ HMH_iOS/HMH_iOS/Presentation/MyPage/ViewModels/MyPageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MyPageViewModel.swift
+//  HMH_iOS
+//
+//  Created by Seonwoo Kim on 4/12/24.
+//
+
+import Foundation
---
+++ HMH_iOS/HMH_iOS/Presentation/MyPage/Views/MyPageContentView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageContentView.swift
+//  HMH_iOS
+//
+//  Created by Seonwoo Kim on 4/12/24.
+//
+
+import SwiftUI
+
+struct MyPageContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPageContentView()
+}
