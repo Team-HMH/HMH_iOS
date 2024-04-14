6b7487805e9bb64ec5ba81468c25014a401f2990
Zoe <mam07065@naver.com>
2024-04-14 22:14:57 +0900

    [Feat/#12] 홈 뷰

--- HMH_iOS/HMH_iOS/Presentation/HomeView.swift
+++ HMH_iOS/HMH_iOS/Presentation/HomeView.swift
@@ -1,19 +1,68 @@
-//
-//  HomeView.swift
-//  HMH_iOS
-//
-//  Created by Seonwoo Kim on 3/12/24.
-//
-
 import SwiftUI
 
+import Lottie
+
 struct HomeView: View {
+    
+    //@StateObject var usageTimeData = HomeViewModel()
+    
     var body: some View {
-        Text("Home")
-            .foregroundColor(.white)
-            .padding()
-            .frame(maxWidth: .infinity, maxHeight: .infinity) 
-            .background(Color.red) 
+        ZStack {
+            Color(.blackground)
+                .ignoresSafeArea()
+            VStack(alignment: .leading) {
+                ZStack(alignment: .topLeading) {
+                    LottieView(animation: .named("Main-A-final.json"))
+                        .playing(loopMode: .autoReverse)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    Text(StringLiteral.Home.usageStatusA)
+                        .font(.text1_medium_22)
+                        .foregroundStyle(.whiteText)
+                        .frame(alignment: .topLeading)
+                        .padding(EdgeInsets(top: 8,
+                                            leading: 20,
+                                            bottom: 0,
+                                            trailing: 0))
+                }
+                Text("목표 사용 시간 3시간 중")
+                    .font(.detail4_medium_12)
+                    .foregroundStyle(.gray2)
+                    .frame(alignment: .leading)
+                    .padding(EdgeInsets(top: 0,
+                                        leading: 20,
+                                        bottom: 0,
+                                        trailing: 0))
+                Text("20분 사용")
+                    .font(.title2_semibold_24)
+                    .foregroundStyle(.whiteText)
+                    .padding(EdgeInsets(top: 2,
+                                        leading: 20,
+                                        bottom: 0,
+                                        trailing: 0))
+                ProgressView(value: 3, total: 10)
+                    .foregroundStyle(.gray5)
+                    .padding(EdgeInsets(top: 0,
+                                        leading: 20,
+                                        bottom: 0,
+                                        trailing: 20))
+                    .tint(.whiteText)
+                
+                
+                
+            }
+        }
+        
+    }
+}
+
+extension HomeView {
+    enum lottie {
+        case statusA 
+        case statusB
+        case statusC
+        case statusD
+        case statusE
     }
 }
 
---
+++ HMH_iOS/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  HMH_iOS
+//
+//  Created by 이지희 on 4/11/24.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+}
