203f35dcca4a7493239b8bb5b761423d3242ca3f
Zoe <mam07065@naver.com>
2024-04-23 23:33:39 +0900

    [Feat/#12] DeviceActivity 타겟 추가

---
+++ HMH_iOS/HMHDeviceActivityReport/HMHDeviceActivityReport.swift
@@ -0,0 +1,20 @@
+//
+//  HMHDeviceActivityReport.swift
+//  HMHDeviceActivityReport
+//
+//  Created by 이지희 on 4/15/24.
+//
+
+import DeviceActivity
+import SwiftUI
+
+@main
+struct HMHDeviceActivityReport: DeviceActivityReportExtension {
+    var body: some DeviceActivityReportScene {
+        // Create a report for each DeviceActivityReport.Context that your app supports.
+        TotalActivityReport { totalActivity in
+            TotalActivityView(totalActivity: totalActivity)
+        }
+        // Add more reports here...
+    }
+}
