fd467e667093fa7cb4d62fe79069fdace4534171
Zoe <mam07065@naver.com>
2024-05-01 12:37:19 +0900

    [Feat/#12] 더미 데이터

--- HMH_iOS/HMHDeviceActivityReport/AppActivityView.swift
+++ HMH_iOS/HMHDeviceActivityReport/AppActivityView.swift
@@ -6,13 +6,41 @@
 //
 
 import SwiftUI
+import FamilyControls
 
 struct AppActivityView: View {
+    let totalActivity: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if let timeString = convertStringToTime(totalActivity) {
+            Text("\(timeString) 사용")
+                .font(.title2_semibold_24)
+                .foregroundStyle(.whiteText)
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
     }
 }
 
+extension AppActivityView {
+    func convertStringToTime(_ string: String) -> String? {
+        let components = string.components(separatedBy: " ")
+        guard components.count == 3,
+              let hours = Int(components[0].replacingOccurrences(of: "h", with: "")),
+              let minutes = Int(components[1].replacingOccurrences(of: "m", with: "")),
+              let seconds = Int(components[2].replacingOccurrences(of: "s", with: "")) else {
+                  return nil
+        }
+
+        let hoursText = String(format: "%02d", hours)
+        let minutesText = String(format: "%02d", minutes)
+        
+        return "\(hoursText)시간 \(minutesText)분"
+    }
+}
+
+// In order to support previews for your extension's custom views, make sure its source files are
+// members of your app's Xcode target as well as members of your extension's target. You can use
+// Xcode's File Inspector to modify a file's Target Membership.
 #Preview {
-    AppActivityView()
+    AppActivityView(totalActivity: "1h 23m")
 }
--- HMH_iOS/HMH_iOS/Presentation/Home/HomeViewModel.swift
+++ HMH_iOS/HMH_iOS/Presentation/Home/HomeViewModel.swift
@@ -5,28 +5,31 @@
 //  Created by 이지희 on 4/11/24.
 //
 
-import Foundation
+import SwiftUI
 
+import FamilyControls
+import DeviceActivity
 import Combine
+import _DeviceActivity_SwiftUI
 
 class HomeViewModel: ObservableObject {
     @Published var totalGoalTime: TimeInterval
     @Published var currentTotalUsage: TimeInterval
     @Published var appsUsage: [AppUsage] = []
-
+    
     init(totalGoalTime: TimeInterval = 3600, currentTotalUsage: TimeInterval = 0, appsUsage: [AppUsage] = []) {
         self.totalGoalTime = totalGoalTime
         self.currentTotalUsage = currentTotalUsage
         self.appsUsage = appsUsage
     }
-
+    
     func remainingTimeForApp(appId: String) -> TimeInterval {
         if let appUsage = appsUsage.first(where: { $0.appId == appId }) {
             return max(appUsage.goalTime - appUsage.usedTime, 0)
         }
         return 0
     }
-
+    
     func updateUsage(forApp appId: String, time: TimeInterval) {
         if let index = appsUsage.firstIndex(where: { $0.appId == appId }) {
             appsUsage[index].usedTime += time
@@ -35,12 +38,12 @@ class HomeViewModel: ObservableObject {
     }
     
     func generateDummyData() {
-            appsUsage = [
-                AppUsage(appId: "1", appName: "Instagram", goalTime: 60, usedTime: 45),
-                AppUsage(appId: "2", appName: "YouTube", goalTime: 45, usedTime: 30),
-                AppUsage(appId: "3", appName: "Twitter", goalTime: 30, usedTime: 15)
-            ]
-        }
+        appsUsage = [
+            AppUsage(appId: "1", appName: "Instagram", goalTime: 60, usedTime: 45),
+            AppUsage(appId: "2", appName: "YouTube", goalTime: 45, usedTime: 30),
+            AppUsage(appId: "3", appName: "Twitter", goalTime: 30, usedTime: 15)
+        ]
+    }
 }
 
 struct AppUsage: Identifiable {
