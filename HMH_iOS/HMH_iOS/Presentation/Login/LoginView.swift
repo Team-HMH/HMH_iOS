e0dad6db0a1109fcc882c0991eaf537b976e68de
kim-seonwoo <sw204011@gmail.com>
2024-05-06 00:35:40 +0900

    [Feat/#19] LoginViewModel Apple Login 추가

--- HMH_iOS/HMH_iOS/Presentation/Login/LoginView.swift
+++ HMH_iOS/HMH_iOS/Presentation/Login/LoginView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 import AuthenticationServices
 
+
 struct LoginView: View {
+    @StateObject private var viewModel = LoginViewModel()
+
     var body: some View {
         VStack {
             SwipeView(imageNames: [.onboardingFirst, .onboardingFirst, .onboardingThird])
-            AppleSigninButton()
-            
+            AppleSigninButton(viewModel: viewModel)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.blackground, ignoresSafeAreaEdges: .all)
@@ -24,36 +26,26 @@ struct LoginView: View {
     LoginView()
 }
 
-struct AppleSigninButton : View{
-    var body: some View{
-        SignInWithAppleButton(
-            onRequest: { request in
-                request.requestedScopes = [.fullName, .email]
-            },
-            onCompletion: { result in
-                switch result {
-                case .success(let authResults):
-                    print("Apple Login Successful")
-                    switch authResults.credential{
-                        case let appleIDCredential as ASAuthorizationAppleIDCredential:
-                           // 계정 정보 가져오기
-                            let UserIdentifier = appleIDCredential.user
-                            let fullName = appleIDCredential.fullName
-                            let name =  (fullName?.familyName ?? "") + (fullName?.givenName ?? "")
-                            let email = appleIDCredential.email
-                            let IdentityToken = String(data: appleIDCredential.identityToken!, encoding: .utf8)
-                            let AuthorizationCode = String(data: appleIDCredential.authorizationCode!, encoding: .utf8)
-                    default:
-                        break
-                    }
-                case .failure(let error):
-                    print(error.localizedDescription)
-                    print("error")
+struct AppleSigninButton: View {
+    @ObservedObject var viewModel: LoginViewModel
+    
+    var body: some View {
+        SignInWithAppleButton { request in
+            request.requestedScopes = [.fullName, .email]
+        } onCompletion: { result in
+            switch result {
+            case .success(let authResults):
+                switch authResults.credential {
+                case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                    viewModel.handleAppleIDCredential(appleIDCredential)
+                default:
+                    break
                 }
+            case .failure(let error):
+                print(error.localizedDescription)
             }
-        )
-        .frame(width : UIScreen.main.bounds.width * 0.9, height:50)
+        }
+        .frame(width: UIScreen.main.bounds.width * 0.9, height: 50)
         .cornerRadius(5)
     }
 }
-
--- HMH_iOS/HMH_iOS/Presentation/Login/ViewModels/LoginViewModel.swift
+++ HMH_iOS/HMH_iOS/Presentation/Login/ViewModels/LoginViewModel.swift
@@ -6,14 +6,44 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
-class LoginViewModel: ObservableObject {
-    
-    @Published
-    var appleLoginInfo = AppleLoginInfo()
-    
-    func loginApple() {
-        
+class LoginViewModel: NSObject, ObservableObject {
+    @Published var appleLoginInfo = AppleLoginInfo()
+
+    func handleAppleLogin() {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.fullName, .email]
+
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.performRequests()
     }
+}
+
+extension LoginViewModel: ASAuthorizationControllerDelegate {
     
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential {
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            handleAppleIDCredential(appleIDCredential)
+        default:
+            break
+        }
+    }
+
+    func handleAppleIDCredential(_ credential: ASAuthorizationAppleIDCredential) {
+        let userIdentifier = credential.user
+//        let fullName = credential.fullName
+//        let name = (fullName?.familyName ?? "") + (fullName?.givenName ?? "")
+//        let email = credential.email
+//        let identityToken = String(data: credential.identityToken ?? Data(), encoding: .utf8)
+//        let authorizationCode = String(data: credential.authorizationCode ?? Data(), encoding: .utf8)
+
+        appleLoginInfo = AppleLoginInfo(token: userIdentifier)
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print(error.localizedDescription)
+    }
 }
