2f5ecba5ee8387f871937ab590aa19b8f583d048
Zoe <mam07065@naver.com>
2024-05-07 00:54:43 +0900

    [Fix/#21] 폴더링

---
+++ HMH_iOS/HMH_iOS/Presentation/Challenge/Views/ChallengeAppListView.swift
@@ -0,0 +1,18 @@
+//
+//  ChallengeAppListView.swift
+//  HMH_iOS
+//
+//  Created by 이지희 on 5/7/24.
+//
+
+import SwiftUI
+
+struct ChallengeAppListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChallengeAppListView()
+}
