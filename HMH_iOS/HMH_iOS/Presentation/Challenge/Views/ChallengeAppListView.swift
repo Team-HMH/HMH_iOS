a33ddd9b78491accda131d3cf3d053c0b0f32ddb
Zoe <mam07065@naver.com>
2024-05-09 02:12:03 +0900

    [Feat/#21] 앱 리스트 뷰 구현

--- HMH_iOS/HMH_iOS/Presentation/Challenge/Views/ChallengeAppListView.swift
+++ HMH_iOS/HMH_iOS/Presentation/Challenge/Views/ChallengeAppListView.swift
@@ -2,14 +2,28 @@
 //  ChallengeAppListView.swift
 //  HMH_iOS
 //
-//  Created by 이지희 on 5/7/24.
+//  Created by 이지희 on 5/6/24.
 //
 
 import SwiftUI
 
 struct ChallengeAppListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "square.fill")
+                .resizable()
+                .frame(width: 40, height: 40)
+                .padding(.trailing, 12)
+                .padding(.leading, 4)
+            Text("앱 이름")
+                .font(.text5_medium_16)
+                .foregroundStyle(.gray2)
+            Spacer()
+            Text("1시간 20분")
+                .font(.text6_medium_14)
+                .foregroundStyle(.whiteText)
+        }
+        .frame(height: 72)
     }
 }
 
