622c78f9ed1db9292bc2878d5b4c696857f63727
Zoe <mam07065@naver.com>
2024-05-10 14:45:19 +0900

    [Add/#26] Challenge DTO

---
+++ HMH_iOS/HMH_iOS/Network/DTO/Challenge/GetDummyResponseDTO.swift
@@ -0,0 +1,24 @@
+//
+//  GetDummyResponseDTO.swift
+//  HMH_iOS
+//
+//  Created by 김보연 on 1/19/24.
+//
+
+import Foundation
+
+struct GetDummyResponseDTO: Codable {
+    let apps: [App]
+}
+
+struct App: Codable {
+    let appName: String
+    let appImageURL: String
+    let goalTime, usageTime: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case appName
+        case appImageURL = "appImageUrl"
+        case goalTime, usageTime
+    }
+}
