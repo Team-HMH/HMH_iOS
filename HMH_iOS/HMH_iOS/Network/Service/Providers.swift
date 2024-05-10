f390b06d7b277de90538c18a0513fafa58f1737f
Zoe <mam07065@naver.com>
2024-05-10 14:48:04 +0900

    [Add/#26] Provider 추가

---
+++ HMH_iOS/HMH_iOS/Network/Service/Providers.swift
@@ -0,0 +1,26 @@
+//
+//  Providers.swift
+//  HMH_iOS
+//
+//  Created by 지희의 MAC on 1/12/24.
+//
+
+import Foundation
+import Moya
+
+struct Providers {
+    static let challengeProvider = NetworkProvider<ChallengeRouter>(withAuth: true)
+    static let myPageProvider = NetworkProvider<MyPageRouter>(withAuth: true)
+    static let AuthProvider = NetworkProvider<AuthRouter>(withAuth: false)
+}
+
+extension MoyaProvider {
+    convenience init(withAuth: Bool) {
+        if withAuth {
+            self.init(session: Session(interceptor: AuthInterceptor.shared),
+                      plugins: [MoyaLoggingPlugin()])
+        } else {
+            self.init(plugins: [MoyaLoggingPlugin()])
+        }
+    }
+}
