ac7cb56cd026423544733059c68c58504aa964ac
Zoe <mam07065@naver.com>
2024-05-12 16:06:52 +0900

    [Feat/#21] 포인트 뷰 추가

---
+++ HMH_iOS/HMH_iOS/Presentation/Challenge/ViewModels/PointViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PointViewModel.swift
+//  HMH_iOS
+//
+//  Created by 이지희 on 5/12/24.
+//
+
+import Foundation
---
+++ HMH_iOS/HMH_iOS/Presentation/Challenge/Views/PointView.swift
@@ -0,0 +1,18 @@
+//
+//  PointView.swift
+//  HMH_iOS
+//
+//  Created by 이지희 on 5/12/24.
+//
+
+import SwiftUI
+
+struct PointView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PointView()
+}
