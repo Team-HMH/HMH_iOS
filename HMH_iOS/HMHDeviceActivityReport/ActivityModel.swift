3f0c0305086c18ca2b2c4391017bcb11a1b3f8a7
Zoe <mam07065@naver.com>
2024-05-13 09:30:48 +0900

    [Feat/#21] 앱 사용 시간용 모델 구현

--- HMH_iOS/HMHDeviceActivityReport/ActivityModel.swift
+++ HMH_iOS/HMHDeviceActivityReport/ActivityModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+import ManagedSettings
+
+struct ActivityReport {
+    let totalDuration: TimeInterval
+    let apps: [AppDeviceActivity]
+}
+
+struct AppDeviceActivity: Identifiable {
+    var id: String
+    var displayName: String
+    var duration: TimeInterval
+    var numberOfPickups: Int
+    var token: ApplicationToken?
+}
+
+extension TimeInterval {
+    /// TimeInterval 타입 값을 00:00 형식의 String으로 변환해주는 메서드
+    func toString() -> String {
+        let time = NSInteger(self)
+        let minutes = (time / 60) % 60
+        let hours = (time / 3600)
+        return String(format: "%0.2d시간 %0.2d분", hours,minutes)
+    }
+}
