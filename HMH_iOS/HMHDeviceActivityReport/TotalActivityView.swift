efbf6899a47eaad0da41d40b71b84c2abea262af
Zoe <mam07065@naver.com>
2024-05-14 13:11:40 +0900

    [Feat/#30] DeviceActivity 타겟 안에서 뷰 구현

--- HMH_iOS/HMHDeviceActivityReport/TotalActivityView.swift
+++ HMH_iOS/HMHDeviceActivityReport/TotalActivityView.swift
@@ -23,23 +23,42 @@ struct TotalActivityView: View {
 extension TotalActivityView {
     func convertStringToTime(_ string: String) -> String? {
         let components = string.components(separatedBy: " ")
-        guard components.count == 3,
-              let hours = Int(components[0].replacingOccurrences(of: "h", with: "")),
-              let minutes = Int(components[1].replacingOccurrences(of: "m", with: "")),
-              let seconds = Int(components[2].replacingOccurrences(of: "s", with: "")) else {
-                  return nil
+
+        // "시간"과 "분"을 분리하여 처리
+        var hours = 0
+        var minutes = 0
+
+        // 각 구성요소를 확인하고, 해당하는 값이 있으면 설정
+        for component in components {
+            if component.contains("h") {
+                if let hourValue = Int(component.replacingOccurrences(of: "h", with: "")) {
+                    hours = hourValue
+                }
+            } else if component.contains("m") {
+                if let minuteValue = Int(component.replacingOccurrences(of: "m", with: "")) {
+                    minutes = minuteValue
+                }
+            }
         }
 
-        let hoursText = String(format: "%d", hours)
-        let minutesText = String(format: "%d", minutes)
-        
-        return "\(hoursText)시간 \(minutesText)분"
+        // 반환 문자열 생성
+        if hours > 0 && minutes > 0 {
+            return "\(hours)시간 \(minutes)분"
+        } else if hours > 0 {
+            return "\(hours)시간"
+        } else if minutes > 0 {
+            return "\(minutes)분"
+        } else {
+            // "시간"이나 "분"이 없는 경우
+            return nil
+        }
     }
 }
+
 
 // In order to support previews for your extension's custom views, make sure its source files are
 // members of your app's Xcode target as well as members of your extension's target. You can use
 // Xcode's File Inspector to modify a file's Target Membership.
-#Preview {
-    TotalActivityView(totalActivity: "1h 23m")
-}
+//#Preview {
+//    TotalActivityView(totalActivity: "1h 23m")
+//}
