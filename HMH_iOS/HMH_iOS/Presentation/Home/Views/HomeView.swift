a32e89ab5421bcac27acf62d342dbbb27052bd90
Zoe <mam07065@naver.com>
2024-05-30 02:39:11 +0900

    [Feat/#40] 홈뷰 요소 이동

--- HMH_iOS/HMH_iOS/Presentation/Home/Views/HomeView.swift
+++ HMH_iOS/HMH_iOS/Presentation/Home/Views/HomeView.swift
@@ -7,7 +7,6 @@ import Lottie
 struct HomeView: View {
     @StateObject var screenTimeViewModel = ScreenTimeViewModel()
     @StateObject var homeViewModel = HomeViewModel()
-    let userDefault = UserDefaults(suiteName: "gorup.HMH")
     
     @State var context: DeviceActivityReport.Context = .init(rawValue: "Total Activity")
     @State var filter = DeviceActivityFilter(
@@ -83,35 +82,8 @@ extension HomeView {
                                             bottom: 0,
                                             trailing: 0))
                     Spacer()
-                    VStack(alignment: .leading) {
-                        Text("목표 사용 시간 3시간 중")
-                            .font(.detail4_medium_12)
-                            .foregroundStyle(.gray2)
-                            .frame(alignment: .leading)
-                            .padding(EdgeInsets(top: 0,
-                                                leading: 20,
-                                                bottom: 0,
-                                                trailing: 0))
-                        HStack {
-                            DeviceActivityReport(context, filter: filter)
-                                .frame(maxHeight: 35, alignment: .leading)
-                            Spacer()
-                            Text("20분 남음")
-                                .font(.detail3_semibold_12)
-                                .foregroundStyle(.whiteText)
-                        }
-                        . padding(EdgeInsets(top: 2,
-                                             leading: 20,
-                                             bottom: 24,
-                                             trailing: 20))
-                        ProgressView(value: 3, total: 10)
-                            .foregroundStyle(.gray5)
-                            .padding(EdgeInsets(top: 0,
-                                                leading: 20,
-                                                bottom: 0,
-                                                trailing: 20))
-                            .tint(.whiteText)
-                    }
+                    DeviceActivityReport(context, filter: filter)
+                        .frame(maxHeight: 83)
                 }
             }
         }
