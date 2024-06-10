55acfc6be723db8c78f06db2f9c99cb2b506893b
Zoe <mam07065@naver.com>
2024-06-11 01:50:17 +0900

    [Feat/#51] 자정 데이터 저장 및 네트워크

--- HMH_iOS/HMH_iOS/Global/Application/NetworkOperator.swift
+++ HMH_iOS/HMH_iOS/Global/Application/NetworkOperator.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import RealmSwift
+
+class NetworkOperation: Operation {
+
+    override func main() {
+        if self.isCancelled { return }
+
+        let semaphore = DispatchSemaphore(value: 0)
+        let midnightDTO = MidnightRequestDTO(finishedDailyChallenges: [])
+        
+        Providers.challengeProvider.request(target: .postDailyChallenge(data: midnightDTO), instance: BaseResponse<EmptyResponseDTO>.self) { result in
+            semaphore.signal()
+        }
+
+        _ = semaphore.wait(timeout: .distantFuture)
+
+        if self.isCancelled { return }
+    }
+}
+
+class RealmUpdateOperation: Operation {
+
+    override func main() {
+        if self.isCancelled { return }
+
+        let realm = try! Realm()
+
+        try! realm.write {
+            let midnightData = MidnightData()
+            midnightData.challengeDate = Date().toString()
+            midnightData.isSuccess = true
+            realm.add(midnightData)
+        }
+
+        if self.isCancelled { return }
+    }
+}
