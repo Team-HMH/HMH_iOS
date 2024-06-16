537add805833cd8423d247328bf80d3a0dfff1fb
Zoe <mam07065@naver.com>
2024-06-17 03:08:08 +0900

    [Fix/#45] 스크린타임 잠금 로직 수정

--- HMH_iOS/DeviceActivityMonitor/DeviceActivityMonitorExtension.swift
+++ HMH_iOS/DeviceActivityMonitor/DeviceActivityMonitorExtension.swift
@@ -37,14 +37,14 @@ class DeviceActivityMonitorExtension: DeviceActivityMonitor {
     
     //threshold에 도착하면 행동한다
     override func eventDidReachThreshold(_ event: DeviceActivityEvent.Name, activity: DeviceActivityName) {
-        store.shield.applications = selectionApps.applicationTokens
+        store.shield.applications = selectionApps.applicationTokens // 이러면 다른 앱도 잠김
         Task {
             await screenTimeVM.handleSetBlockApplication()
         }
         let notiContent = UNMutableNotificationContent()
         notiContent.title = "하면함"
         notiContent.body = "이용 시간이 종료 되었습니다."
-        notiContent.userInfo = ["status": "unblockApp"] // 푸시 받을때 오는 데이터
+        notiContent.userInfo = ["token": ""] // 푸시 받을때 오는 데이터
         
         // 알림이 trigger되는 시간 설정
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
--- HMH_iOS/HMH_iOS/Presentation/Challenge/ViewModels/ScreenTimeViewModel.swift
+++ HMH_iOS/HMH_iOS/Presentation/Challenge/ViewModels/ScreenTimeViewModel.swift
@@ -107,9 +107,7 @@ final class ScreenTimeViewModel: ObservableObject {
         let schedule = DeviceActivitySchedule(
             intervalStart: DateComponents(hour: dateComponents.hour, minute: dateComponents.minute, second: dateComponents.second),
             intervalEnd: DateComponents(hour: 23, minute: 59, second: 59),
-            repeats: false,
-            //warning Time 설정해야 알람
-            warningTime: DateComponents(minute: minute - 1 ) // 여기는 전체 시간 가까워질 때
+            repeats: true
         )
          //새 이벤트 생성
         let event = DeviceActivityEvent(
