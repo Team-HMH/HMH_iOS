0c93b2661da79eb760adcde360a716736132c258
Zoe <mam07065@naver.com>
2024-11-01 03:02:20 +0900

    [Feat/#97] 챌린지 뷰 리팩토링

--- HMH_Tuist_iOS/Projects/Features/ChallengeFeature/Sources/ViewModels/ChallengeViewModel.swift
+++ HMH_Tuist_iOS/Projects/Features/ChallengeFeature/Sources/ViewModels/ChallengeViewModel.swift
@@ -9,176 +9,53 @@ import SwiftUI
 import FamilyControls
 import Domain
 
-enum ChallengeType {
-    case empty
-    case normal
-    case large
+/// 추후 도메인 계층으로 분리할 모델
+struct Challenge {
+  let startDate: String
+  let visableStartDate: String
+  let todayIndex: Int
+  let days: Int
+  let statuses: [String]
 }
+
 
 public final class ChallengeViewModel: ObservableObject {
-    @Published var startDate = ""
-    @Published var visableStartDate = ""
-    @Published var todayIndex = 0
-    @Published var days = 7
-    @Published var appList: [AppInfo] = []
-    @Published var statuses: [String] = []
-    @Published var titleString = ""
-    @Published var subTitleString = ""
-    @Published var challengeType: ChallengeType = .empty
-    @Published var remainEarnPoint = 0
-    @Published var navigateToCreate = false
-    @Published var isToastPresented = false
-    
-    @StateObject var screenViewModel = ScreenTimeViewModel()
-    
-    enum PointStatus {
-        static let unearned = "UNEARNED"
-        static let earned = "EARNED"
-        static let failure = "FAILURE"
-    }
-    
-    public init() {
-        getChallengeInfo()
-    }
-    
-    func getChallengeType() {
-        if todayIndex < 0 {
-            challengeType = .empty
-        } else if 14 < days {
-            challengeType = .large
-        } else {
-            challengeType = .normal
-        }
-    }
-    
-    //TODO: 네트워크 부분은 의존성 정리한 뒤에 다시 연결해봅시다
-    func getChallengeInfo() {
-//        Providers.challengeProvider.request(target: .getChallenge,
-//                                            instance: BaseResponse<GetChallengeResponseDTO>.self) { result in
-//            guard let data = result.data else { return }
-//            self.days = data.period
-//            self.appList = data.apps
-//            self.statuses = data.statuses
-//            self.todayIndex = data.todayIndex
-//            self.startDate = data.startDate
-//            self.visableStartDate = self.formatDateString(data.startDate) ?? ""
-//            
-//            self.sendSucessIfNeeded()
-//            self.getChallengeType()
-//        }
-    }
-    
-    func challengeButtonTapped() {
-        if !(statuses.contains("UNEARNED")) {
-            navigateToCreate = true
-        } else {
-            isToastPresented = true
-        }
-    }
-    
-    //TODO: 네트워크 부분은 의존성 정리한 뒤에 다시 연결해봅시다
-    func addApp(appGoalTime: Int) {
-//        var applist: [Apps] = []
-//        
-//        screenViewModel.selectedApp.applications.forEach { app in
-//            applist.append(Apps(appCode: app.localizedDisplayName ?? "basic name", goalTime: appGoalTime))
-//        }
-//        
-//        screenViewModel.handleStartDeviceActivityMonitoring(includeUsageThreshold: true, interval: appGoalTime)
-//        
-//        Providers.challengeProvider.request(target: .addApp(data: AddAppRequestDTO(apps: applist)), instance: BaseResponse<EmptyResponseDTO>.self) { result in
-//            print(result)
-//        }
-        
-    }
-    
-    func formatDateString(_ dateString: String) -> String? {
-        let inputDateFormatter = DateFormatter()
-        inputDateFormatter.dateFormat = "yyyy-MM-dd"
-        guard let date = inputDateFormatter.date(from: dateString) else {
-            return nil
-        }
-        
-        let outputDateFormatter = DateFormatter()
-        outputDateFormatter.dateFormat = "M월 d일"
-        let formattedDateString = outputDateFormatter.string(from: date)
-        
-        return formattedDateString
-    }
-    
-    //TODO: 네트워크 부분은 의존성 정리한 뒤에 다시 연결해봅시다
-    func sendFailChallenge(date: String) {
-//        let midnightDTO = MidnightRequestDTO(finishedDailyChallenges: [FinishedDailyChallenge(challengeDate: date, isSuccess: false)])
-//        Providers.challengeProvider.request(target: .postDailyChallenge(data: midnightDTO), instance: BaseResponse<EmptyResponseDTO>.self) { result in
-//            print("Daily challenge data sent successfully.")
-//        }
-    }
-    
-    //TODO: 네트워크 부분은 의존성 정리한 뒤에 다시 연결해봅시다
-    func sendSucessIfNeeded() {
-//        let noneDates = findNoneDates(statuses: statuses, todayIndex: todayIndex, startDate: startDate)
-//        var finishChallenges: [FinishedDailyChallenge] = []
-//        
-//        noneDates.forEach { date in
-//            finishChallenges.append(FinishedDailyChallenge(challengeDate: date, isSuccess: true))
-//        }
-//        
-//        if !(finishChallenges.isEmpty) {
-//            let finishDateDTO = MidnightRequestDTO(finishedDailyChallenges: finishChallenges)
-//            
-//            Providers.challengeProvider.request(target: .postDailyChallenge(data: finishDateDTO), instance: BaseResponse<EmptyResponseDTO>.self) { result in
-//                print("Daily challenge data sent successfully.")
-//            }
-//        }
-    }
-    
-    func findNoneDates(statuses: [String], todayIndex: Int, startDate: String) -> [String] {
-        var dates: [String] = []
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        
-        guard let start = dateFormatter.date(from: startDate) else {
-            print("Invalid start date format")
-            return dates
-        }
-        
-        let calendar = Calendar.current
-        
-        if todayIndex > 0 {
-            for index in 0..<todayIndex {
-                if statuses[index] == "NONE" {
-                    if let newDate = calendar.date(byAdding: .day, value: index, to: start) {
-                        let formattedDate = dateFormatter.string(from: newDate)
-                        dates.append(formattedDate)
-                    }
-                }
-            }
-        } else {
-            for index in 0..<days {
-                if statuses[index] == "NONE" {
-                    if let newDate = calendar.date(byAdding: .day, value: index, to: start) {
-                        let formattedDate = dateFormatter.string(from: newDate)
-                        dates.append(formattedDate)
-                    }
-                }
-            }
-        }
-        
-        return dates
-    }
-}
+  /// 챌린지 정보
+  @Published var challenge: Challenge = .init(startDate: "", visableStartDate: "", todayIndex: 0, days: 0, statuses: [])
+  /// 챌린지 존재여부  - 챌린지 생성 버튼
+  @Published var isChallengeExisted: Bool = false
+  
+  /// 뷰에 보여질 부분
+  @Published var titleString = ""
+  @Published var subTitleString = ""
+  
+  /// 뷰 이동과 관련된 코드
+  @Published var navigateToCreate = false
+  @Published var navigateToPoint = false
+  @Published var isToastPresented = false
+  
 
-struct ChallengeDTO: Codable {
-    let period: Int
-    let statuses: [String]
-    let todayIndex: Int
-    let goalTime: Int
-    let apps: [AppInfo]
-}
-
-// MARK: - App
-struct AppInfo: Codable {
-    let appCode: String
-    let goalTime: Int
+  public init() {
+    getChallengeInfo()
+  }
+  
+  func getChallengeInfo() {
+    // 네트워크 요청 로직 분리 후 Challenge 데이터 처리
+    // 예: challengeService.fetchChallenge() { [weak self] result in ... }
+    self.isChallengeExisted = checkChallengeExistence()
+  }
+  
+  func challengeButtonTapped() {
+    // 안 받은 포인트가 있을 때는 토스트 메시지
+    if challenge.statuses.contains("UNEARNED") {
+      isToastPresented = true
+    } else {
+      // 모든 포인트를 받았다면 navigate
+      navigateToPoint = true
+    }
+  }
+  
+  private func checkChallengeExistence() -> Bool {
+    return challenge.days > 0
+  }
 }
