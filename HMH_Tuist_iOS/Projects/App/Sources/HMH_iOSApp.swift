37b0c4a7232925d8309de4eced0a3b3ec42c9704
Zoe <mam07065@naver.com>
2024-11-16 03:44:04 +0900

    [Feat/#104] 코디네이터 적용으로 ContentView AppView로 변경

--- HMH_Tuist_iOS/Projects/App/Sources/HMH_iOSApp.swift
+++ HMH_Tuist_iOS/Projects/App/Sources/HMH_iOSApp.swift
@@ -14,7 +14,6 @@ import KakaoSDKAuth
 struct HMH_iOSApp: App {
     let kakaoAPIKey = Bundle.main.infoDictionary?["KAKAO_API_KEY"] as! String
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    @StateObject private var scheduler = MidnightTaskScheduler()
     @Environment(\.scenePhase) private var scenePhase
 
     init() {
@@ -23,16 +22,7 @@ struct HMH_iOSApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-        }
-        .onChange(of: scenePhase) { newPhase in
-            switch newPhase {
-            case .background:
-                print("App moved to background.")
-                scheduler.scheduleMidnightTask()
-            @unknown default:
-                break
-            }
+            AppView()
         }
     }
 }
